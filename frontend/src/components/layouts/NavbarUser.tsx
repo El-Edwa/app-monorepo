d64c615c796ba57e73eeea8982687c9e67d94ad6
Moaz Ayman <moaz.ayman.mahmoud@gmail.com>
2025-06-25 16:00:32 +0300

    fix(NavbarUser): enhance layout and styling for improved user interaction and responsiveness

--- frontend/src/components/layouts/NavbarUser.tsx
+++ frontend/src/components/layouts/NavbarUser.tsx
@@ -2,24 +2,21 @@ import { IoIosMore } from "react-icons/io";
 
 export default function NavbarUser() {
   return (
-    <div className="flex justify-between items-start py-3 gap-8 cursor-pointer hover:bg-neutral-900 rounded-full transition-colors duration-200">
-      <div className="flex items-center gap-2">
+    <div className="flex justify-between sm:ml-3 items-start py-3 gap-8 cursor-pointer">
+      <div className="flex items-center gap-2 min-w-0 flex-1">
         {/* User Image */}
         <img
           src="https://content.imageresizer.com/images/memes/Badass-Skeleton-meme-2d5o0i.jpg"
           alt="user"
-          className="w-12 h-12 rounded-full object-cover cursor-pointer"
+          className="w-12 h-12 rounded-full object-cover flex-shrink-0"
         />
         {/* User Name and Handle */}
-        <div className="hidden xl:block">
+        <div className="hidden xl:block min-w-0 flex-1">
           <p className="font-bold truncate">Skeleton</p>
-          <p className="text-neutral-500 text-sm">@death</p>
+          <p className="text-neutral-500 text-sm truncate">@death</p>
         </div>
       </div>
-      <IoIosMore
-        size={"1.5em"}
-        className="hidden xl:block ml-4 mt-4 cursor-pointer"
-      />
+      <IoIosMore size={"1.5em"} className="hidden xl:block ml-4 mt-4 flex-shrink-0" />
     </div>
   );
 }
